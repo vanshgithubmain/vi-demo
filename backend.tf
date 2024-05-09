02029eca5168202c6e07c9454bfa1eee98e626ca
vansh0766 <vansh0766.be20@chitkara.edu.in>
2024-05-10 02:13:22 +0530

    first com3

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "heena123987"
-#     key            = "terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "table-store"
-#   }
-# }
